510666ed8af310aa809bc8f8b6902897657aaff0
elyelamae <144203300+elyelamae@users.noreply.github.com>
2024-09-07 08:33:50 +0800

    Add files via upload

---
+++ Grp5_mendoza_ex1.py
@@ -0,0 +1,23 @@
+print("Hello, I'm Ellyzian Angela Mae Mendoza BSCS 3F")
+
+Age = 24
+Name = "Ellyzian"
+
+print(Age)
+print(Name)
+
+Elopre = "Elopre"
+Judith = "Judith"
+Irene  = "Irene"
+Diana = "Diana"
+
+print (Elopre)
+print (Judith)
+print (Irene)
+print (Diana)
+
+Name1, Name2, Name3 = "Ayen", "Jemuel", "Clark"
+print(Name1,Name2,Name3)
+
+x = y = z = 50
+print(x, y, z)
